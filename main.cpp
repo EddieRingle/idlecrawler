51cdff7f42ab77c49a5dfc0f4a8203f00a27dc2b
Eddie Ringle <eddie@eringle.net>
2009-10-15 18:42:11 -0400

    main.cpp: Added command-line arguments.
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- main.cpp
+++ main.cpp
@@ -17,11 +17,19 @@ int main(int argc, char **argv)
 {
 	int cc_err;
 	Crawler *bot = new Crawler();
-	cc_err = bot->Connect("eddieringle.com",80);
+	if (argc >= 2) {
+		cc_err = bot->Connect(argv[1],80);
+	} else {
+		cc_err = bot->Connect("eddieringle.com",80);
+	}
 	if (cc_err != 0)
 		return cc_err;
-	cc_err = bot->Fetch("/");
+	if (argc >= 3) {
+		cc_err = bot->Fetch(argv[2]);
+	} else {
+		cc_err = bot->Fetch("/");
+	}
 	if (cc_err != 0)
 		return cc_err;
-	return(EXIT_SUCCESS);
+	return 0;
 }
